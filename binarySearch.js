64f251dad50202b8ef0786d09d9328f3ace5d59e
yevmnat <evgine.v.mnatsakanyan@gmail.com>
2022-10-07 16:47:17 +0400

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,21 @@
+
+function solution(nums, target) {
+    if (!Array.isArray(nums) || typeof target != "number") {
+        return "Invalid arguments"
+    }
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
