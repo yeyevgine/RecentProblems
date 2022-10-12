a970437faf275c2350b6b070187ec38b4f6e9d2b
yevmnat <evgine.v.mnatsakanyan@gmail.com>
2022-10-12 19:43:34 +0400

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,1 +1,15 @@
 
+function insertionSort(arr) {
+    let currentVal, j;
+    for (let i = 1; i < arr.length; i++) {
+        currentVal = arr[i];
+        j = i - 1;
+
+        while ((j >= 0) && (arr[j] > currentVal)) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr;
+}
