456e7a514e679131c7b631dbe4e0b9b90db5e480
yevmnat <evgine.v.mnatsakanyan@gmail.com>
2022-10-12 20:07:28 +0400

    Homework for 13.10.22

--- subString.js
+++ subString.js
@@ -1,1 +1,22 @@
 
+function subStr(string, substr){
+    if(string.length < substr.length){
+        return -1;
+    }
+    let index = -1;
+    for (let i = 0; i <= string.length ; ++i) {
+        if(string[i] === substr[0]){
+            index = i;
+            for (let j = 1; j < substr.length; ++j) {
+                if(string[i + j] !== substr[j]){
+                    index = -1;
+                    break;
+                }
+            }
+            if(index !== -1){
+                return index;
+            }
+        }
+    }
+    return index;
+}
