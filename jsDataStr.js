2bdda3e17208629ce28e6f8b156df056bf91cc5b
yevmnat <evgine.v.mnatsakanyan@gmail.com>
2022-10-13 11:12:58 +0400

    Practice from JSDataStrBook. / 13.10.22

--- jsDataStr.js
+++ jsDataStr.js
@@ -1,3 +1,4 @@
+//Matrix
 function Matrix(rows, columns) {
     let jaggedarray = new Array(rows);
     for(let i = 0; i < columns; ++i) {
@@ -5,3 +6,34 @@ function Matrix(rows, columns) {
     }
     return jaggedarray;
 }
+
+//Spiral Print
+function spiralPrint(matrix) {
+    let topRow = 0;
+    let leftCol = 0;
+    let bottomRow = matrix.length - 1;
+    let rightCol = matrix[0].length - 1;
+
+    while (topRow < bottomRow && leftCol < rightCol) {
+        for (let col = 0; col <= rightCol; ++col) {
+            console.log(matrix[topRow][col]);
+        }
+        topRow++;
+        for (let row = topRow; row <= bottomRow; ++row) {
+            console.log(matrix[row][rightCol]);
+        }
+        rightCol--;
+        if (topRow <= bottomRow) {
+            for (let col = rightCol; col > 0; --col) {
+                console.log(matrix[bottomRow][col]);
+            }
+            bottomRow--;
+        }
+        if (leftCol <= rightCol) {
+            for (let row = bottomRow; row > topRow; --row) {
+                console.log(matrix[row][leftCol]);
+            }
+            leftCol++;
+        }
+    }
+} 
